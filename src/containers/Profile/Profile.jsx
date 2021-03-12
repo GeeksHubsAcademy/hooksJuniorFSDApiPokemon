e7a49215074450105833a2e5e5d731e957d7bc39
dave86dev <davidochando1986@gmail.com>
2021-03-12 13:28:03 +0100

    Ant design and final touches

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -1,7 +1,10 @@
 
 import React, {useState,useEffect} from 'react';
 import axios from 'axios';
+import {Button, DatePicker} from "antd";
 
+import 'antd/lib/button/style/index.css';
+import 'antd/lib/date-picker/style/index.css';
 import './Profile.css';
 
 const Profile = () => {
@@ -10,7 +13,8 @@ const Profile = () => {
 
     const [pokemons, setPokemon] = useState({
         monsters : [],
-        episodes : []
+        episodes : [],
+        pokemonNumber : 1
     });
 
     //useEffect
@@ -18,7 +22,7 @@ const Profile = () => {
     useEffect(  () => {
 
         const fetchData = async () => {
-            let dataApi = await axios.get('https://pokeapi.co/api/v2/pokemon/5/');
+            let dataApi = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemons.pokemonNumber}/`);
             
             setTimeout(()=>{
                 
@@ -35,57 +39,75 @@ const Profile = () => {
 
     useEffect(()=>{
 
-        // if(pokemons.monsters?.name){
-            
-        //     console.log("Los datos han cambiado??", pokemons.monsters);
-            
-
-
-        // }
+        console.log(pokemons.pokemonNumber);
+        
     });
 
     //Handlers
 
     //Funciones 
 
+    const changePokemon = async (int) => {
+
+        if(pokemons.pokemonNumber + int > 0){
+            
+            let updatedPokemon = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemons.pokemonNumber + int}/`);
+
+            setPokemon({...pokemons, pokemonNumber : pokemons.pokemonNumber + int, monsters : updatedPokemon.data}); 
+        }
+
+        return;
+    }
+
     if(!pokemons.monsters?.name){
 
         
         return(
             <div>
-                <img src="img/loader.gif" className="spinner"/>
+                <img src="img/spinnerCustom.gif" className="spinner"/>
             </div>
         );
     }else {
         //Mapeamos una vez tenemos la confirmación de que los datos existen
-        console.log(pokemons.monsters);
-        return(<div className="cardPokemon">
-            
-            <div className="pokemonUp">
-                {pokemons.monsters?.name}
-            </div>
-            <div className="pokemonMiddle">
-               
-                <div>
-                    <img src={pokemons.monsters.sprites?.front_shiny}/>
-                </div>
-                <div>
-                    <img src={pokemons.monsters.sprites?.back_default}/>
-                </div>
-            </div>
-            <div className="pokemonDown">
-                {pokemons.monsters?.abilities.map(habilidad => {
+        
+        return(
+        
+            <div className="profileContainer">
+
+                <div className="cardPokemon">
+                    <div className="logoPokemon">
+                        <img className="awfulLogo" src="img/pokemon.png"/>
+                    </div>
+                    <div className="pokemonUp">
+                        {pokemons.monsters?.name}
+                    </div>
+                    <div className="pokemonMiddle">
+                
+                        <div>
+                            <img src={pokemons.monsters.sprites?.front_shiny}/>
+                        </div>
+                        <div>
+                            <img src={pokemons.monsters.sprites?.back_default}/>
+                        </div>
+                    </div>
+                    <div className="pokemonDown">
+                        {pokemons.monsters?.abilities.map(habilidad => {
+                        
+                            return(
+                                <div className="habilidadPokemon">
+                                    {habilidad.ability?.name}
+                                    </div>
+                            )
+                        })}
+                    </div>
+                   
+                    <Button onClick={()=> changePokemon(-1)}>Previous</Button>
+                    <Button onClick={()=> changePokemon(+1)}>Next</Button>
                     
-                    return(
-                        <div className="habilidadPokemon">
-                            {habilidad.ability?.name}
-                            </div>
-                    )
-                })}
-            </div>
-            
+                </div>
 
             </div>
+        
         );
     }
 
